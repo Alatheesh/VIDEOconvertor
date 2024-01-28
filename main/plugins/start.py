4a44419cf6c8d767e5cba5a99f678980d2186415
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2024-01-28 11:13:59 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -49,8 +49,8 @@ async def notice(event):
 async def source(event):
     await event.edit(source_text,
                     buttons=[[
-                         Button.url("FOR PERSONAL USE", url="https://github.com/vasusen-code/videoconvertor/tree/main"),
-                         Button.url("FOR YOUR CHANNEL ", url="https://github.com/vasusen-code/videoconvertor/")]])
+                         Button.url("DEVOPS", url="https://t.me/TG_LATHEESH"),
+                         Button.url("MAIN", url="https://t.me/llathu63035")]])
                          
                     
 @Drone.on(events.callbackquery.CallbackQuery(data="help"))
