298f43623a041e267594eddb64b30390391e7f3a
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2024-01-30 22:50:47 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -62,7 +62,7 @@ async def help(event):
                          [
                          Button.inline("PLUGINS", data="plugins"),
                          Button.inline("RESTART", data="restart")],
-                         [Button.url("SUPPORT", url=f"{SUPPORT_LINK}")],
+                         [Button.url("SUPPORT", url=f"t.me/llathu63035")],
                          [
                          Button.inline("BACK", data="menu")]])
     
